071a2d34441d17ed9434a6db3fa7e539b69ddcb9
SachinITTec <109853518+SachinITTec@users.noreply.github.com>
2022-07-24 19:08:43 +0530

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,70 @@
+#EX1:-
+# var1=int(input())
+# var2=50
+
+# if var1>var2:
+#     print("the value is greater")
+# else:
+#     print("the value is smaller")
+
+#EX2:-
+# var1=int(input("enter the first value::"))
+# var2=int(input("enter th second value::"))
+# var3=100
+
+# if (var1+var2)>var3:
+#     print("the value is greater than 100")
+# else:
+#     print("the value is smaller value")
+
+#EX3:-
+# print("What is your age::..")
+# Age=int(input("enter your age::"))
+# if Age>18:
+#     print("you can drive the car..")
+# elif Age==18:
+#     print("you can drive the car..")
+# else:
+#     print("you can't drive the car..")
+
+#EX4:-
+# list1=[1,2,3,4]
+# print(17 not in list1)
+# if 17 not in list1:
+#     print("no it is not in he list1")
+# else:
+#     print("it is in the list")
+
+#exercise:-
+var1=int(input("enter the 1st no::"))
+var2=int(input("enter the2nd number::"))
+print("enter 1 for multiplication","enter 2 for the division","enter 3 for the addition")
+
+# fraction1=var1+var2
+# print("the addition is:-",fraction1)
+
+# fraction2=var1-var2
+# print("the substraction is:-",fraction2)
+
+# fraction3=var1*var2
+# print("the multiplication is:-",fraction3)
+
+# fraction4=var1/var2
+# print("the division is:-",fraction4)
+c=int(input("enter the choice:"))
+if c==1:
+        if var1==45 and var2==3:
+            print("555")
+        else:print(var1*var2)   
+elif c==2:
+    if var1==56 and var2==4:
+        print(4)
+    else:
+        print(var1/var2)           
+elif c==2:
+    if var1==56 and var2==9:
+        print(77)
+    else:
+        print(var1+var2)     
+
+
